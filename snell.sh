d663560f4c9ea28cd9eb024200ab2e29b6bd06b2
NinjaOSX <amiao.vip@gmail.com>
2020-12-16 21:46:54 +0800

    Update snell.sh

--- snell.sh
+++ snell.sh
@@ -1,18 +1,37 @@
+debug()
+{
+echo "Press any key to continue or Press ctrl+c to exit..."
+read anything
+}
+
 apt install apt-get -y
+debug
+
 apt-get install wget -y
+debug
 
 yum install wget -y
+debug
 
 wget --no-check-certificate -O snell.zip https://github.com/surge-networks/snell/releases/download/v2.0.3/snell-server-v2.0.3-linux-amd64.zip
+debug
+
 wget --no-check-certificate -O snell.service https://github.com/surge-networks/snell/raw/master/systemd-example
+debug
 
 #wget --no-check-certificate -O snell.service https://raw.githubusercontent.com/NinjaOSX/snl/main/systemd-example
 
 apt-get install unzip -y
+debug
+
 yum install unzip -y 
+debug
 
 unzip -o -q snell.zip
+debug
+
 rm -f snell.zip
+debug
 
 # echo "y" > snell.new.config #写入y
 # chmod +x snell-server
@@ -20,21 +39,26 @@ rm -f snell.zip
 # rm -f snell.new.config 
 
 systemctl stop snell.service
+debug
 
 chmod +x snell-server
-yes=“y”
-./snell-server < $yes
+debug
 
-echo "press any key to continue"
-read anything
+yes=“y”
+debug
+
+./snell-server < $yes
+debug
 
 mv -f snell-server /usr/local/bin/
 mv -f snell-server.conf /etc/
 mv -f snell.service /lib/systemd/system/
+debug
 
 sudo systemctl daemon-reload
 sudo systemctl enable snell.service
 sudo systemctl start snell.service
+debug
 
 #clear
 cat /etc/snell-server.conf
