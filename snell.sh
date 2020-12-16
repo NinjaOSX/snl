b3b8bf1e0094c31943d8ea08b7c9d25e133f722d
NinjaOSX <amiao.vip@gmail.com>
2020-12-16 12:59:53 +0800

    Create snell.sh

---
+++ snell.sh
@@ -0,0 +1,18 @@
+wget --no-check-certificate -O snell.zip https://github.com/surge-networks/snell/releases/download/v2.0.3/snell-server-v2.0.3-linux-amd64.zip
+wget --no-check-certificate -O snell.service https://github.com/surge-networks/snell/raw/master/systemd-example
+yum install unzip -y 
+apt-get install unzip -y
+unzip -o -q snell.zip
+rm -f snell.zip
+echo "y" > snell.new.config
+chmod +x snell-server
+./snell-server < snell.new.config 
+rm -f snell.new.config 
+mv -f snell-server /usr/local/bin/
+mv -f snell-server.conf /etc/
+mv -f snell.service /lib/systemd/system/
+sudo systemctl daemon-reload
+systemctl enable snell.service
+systemctl start snell.service
+systemctl status snell.service
+cat /etc/snell-server.conf
