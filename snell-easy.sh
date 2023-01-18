d794ba59e02ff7dc91a80eb026cdb99c99901731
NinjaOSX <amiao.vip@gmail.com>
2023-01-18 23:19:26 +0800

    Update snell-easy.sh

--- snell-easy.sh
+++ snell-easy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+#PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 #export PATH
 
 apt-get update
