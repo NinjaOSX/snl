b95ffa4b8b14efa7bf90cedda271bc8732d69b29
NinjaOSX <amiao.vip@gmail.com>
2023-01-19 13:28:01 +0800

    Update snl.sh

--- snl.sh
+++ snl.sh
@@ -1,7 +1,7 @@
-crul -s -o snell.tar https://github.com/NinjaOSX/snl/releases/download/V4.0.0/snell-server-v4.0.0-linux-amd64.tar
-crul -s -o snell.service https://raw.githubusercontent.com/NinjaOSX/snl/main/systemd-example
+curl -s -o snell.tar https://github.com/NinjaOSX/snl/releases/download/V4.0.0/snell-server-v4.0.0-linux-amd64.tar
+curl -s -o snell.service https://raw.githubusercontent.com/NinjaOSX/snl/main/systemd-example
 
-tar -x snell.tar
+tar -xf snell.tar
 rm -f snell.tar
 
 echo "y" > snell.yes #写入y
